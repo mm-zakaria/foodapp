dce2657b5fbdc636ad00009056e6d4e263442590
mm-zakaria <mmzakarianwucse103@gmail.com>
2024-07-28 23:13:00 +0600

    adda extra unknown feature

--- food/lib/pages/my_ads.dart
+++ food/lib/pages/my_ads.dart
@@ -44,3 +44,13 @@ class MyAds extends StatelessWidget {
     );
   }
 }
+
+
+var name = 'Voyager I';
+var year = 1977;
+var antennaDiameter = 3.7;
+var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+var image = {
+  'tags': ['saturn'],
+  'url': '//path/to/saturn.jpg'
+};
