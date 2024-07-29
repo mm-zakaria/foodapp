ab7b28f27f2217a98bdf6d2367e3d1db63f6614b
mm-zakaria <mmzakarianwucse103@gmail.com>
2024-07-29 17:49:41 +0600

    work on add button

--- food/lib/pages/add.dart
+++ food/lib/pages/add.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
 
 class Add extends StatelessWidget {
+  TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('add'),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        print(_controller.text);
+        _controller.clear();
+      }),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          TextField(
+            controller: _controller,
+            decoration: InputDecoration(
+                prefix: IconButton(
+                    onPressed: () {}, icon: Icon(Icons.alarm_add_rounded)),
+                     suffix: IconButton(
+                    onPressed: () {}, icon: Icon(Icons.search)),
+                    
+                    ),
+                    
+          ),
+          TextFormField(),
+         
+        ],
       ),
     );
   }
--- food/lib/pages/my_ads.dart
+++ food/lib/pages/my_ads.dart
@@ -46,17 +46,3 @@ class MyAds extends StatelessWidget {
 }
 
 
-var name = 'Voyager I';
-var year = 1977;
-var antennaDiameter = 3.7;
-var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
-var image = {
-  'tags': ['saturn'],
-  'url': '//path/to/saturn.jpg'
-};
-
-var antennaDiameter = 3.7;
-var image = {
-  'tags': ['saturn'],
-  'url': '//path/to/saturn.jpg'
-};
